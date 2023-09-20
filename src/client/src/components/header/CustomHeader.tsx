5db891f1af1650ddbfa0ea529d94924a7c0a6b45
Eesha Moona <eeshamoona@gmail.com>
2023-09-20 08:17:14 -0500

    Prettified custom header

--- src/client/src/components/header/CustomHeader.tsx
+++ src/client/src/components/header/CustomHeader.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./customHeader.scss";
-import { IconSearch } from "@tabler/icons-react";
-import { IconLockAccess } from "@tabler/icons-react";
-import { IconBell } from "@tabler/icons-react";
+import {
+  IconSearch,
+  IconLockAccess,
+  IconBell,
+  IconSun,
+  IconMoonStars,
+} from "@tabler/icons-react";
 import {
   ActionIcon,
   UnstyledButton,
   Indicator,
   Avatar,
   Box,
+  TextInput,
+  useMantineTheme,
+  useMantineColorScheme,
 } from "@mantine/core";
-import { TextInput } from "@mantine/core";
 import AddButton from "../add_button/AddButton";
-import { useMantineTheme } from "@mantine/core";
-import { useMantineColorScheme } from "@mantine/core";
-import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
 interface HeaderProps {
   changeTabCallback: (tab: string) => void;
@@ -33,50 +36,40 @@ const Header = ({ changeTabCallback }: HeaderProps) => {
       </UnstyledButton>
 
       <div id="header-right-container">
-        <div id="search-bar-container">
-          <TextInput
-            placeholder="Search"
-            id="search-bar"
-            rightSection={
-              <Box id="search-icon-background">
-                <IconSearch id="search-icon" color="gray" />
-              </Box>
-            }
-          />
-        </div>
-        <div id="add-think-button-container">
-          <AddButton changeTabCallback={changeTabCallback} />
-        </div>
-        <div id="notification-button-container">
-          <ActionIcon
-            id="notification-button"
-            c={theme.colorScheme === "dark" ? "cream" : "slategray"}
-          >
-            <Indicator color="red" size={".75rem"}>
-              <IconBell id="bell-icon" size={"1.5rem"} />
-            </Indicator>
-          </ActionIcon>
-        </div>
-        <div id="dark-mode-toggle-container">
-          <ActionIcon
-            id="notification-button"
-            c={theme.colorScheme === "dark" ? "yellow" : "blue"}
-            onClick={() => toggleColorScheme()}
-          >
-            {dark ? (
-              <IconSun size={"1.5rem"} />
-            ) : (
-              <IconMoonStars size={"1.5rem"} />
-            )}
-          </ActionIcon>
-        </div>
-        <div id="user-profile-button-container">
-          <ActionIcon id="user-profile-button">
-            <Avatar color="violet" id="user-profile">
-              EM
-            </Avatar>
-          </ActionIcon>
-        </div>
+        <TextInput
+          placeholder="Search"
+          id="search-bar"
+          rightSection={
+            <Box id="search-icon-background">
+              <IconSearch id="search-icon" color="gray" />
+            </Box>
+          }
+        />
+        <AddButton changeTabCallback={changeTabCallback} />
+        <ActionIcon
+          id="notification-button"
+          c={theme.colorScheme === "dark" ? "cream" : "slategray"}
+        >
+          <Indicator color="red" size={".75rem"}>
+            <IconBell id="bell-icon" size={"1.5rem"} />
+          </Indicator>
+        </ActionIcon>
+        <ActionIcon
+          id="notification-button"
+          c={theme.colorScheme === "dark" ? "yellow" : "blue"}
+          onClick={() => toggleColorScheme()}
+        >
+          {dark ? (
+            <IconSun size={"1.5rem"} />
+          ) : (
+            <IconMoonStars size={"1.5rem"} />
+          )}
+        </ActionIcon>
+        <ActionIcon id="user-profile-button">
+          <Avatar color="violet" id="user-profile">
+            EM
+          </Avatar>
+        </ActionIcon>
       </div>
     </div>
   );
