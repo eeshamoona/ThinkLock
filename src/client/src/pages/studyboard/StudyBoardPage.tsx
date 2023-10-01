810d8b08ec0b761ef6efff032e9987958e63c04a
Eesha Moona <eeshamoona@gmail.com>
2023-09-30 23:45:59 -0500

    Further storyboard styles

--- src/client/src/pages/studyboard/StudyBoardPage.tsx
+++ src/client/src/pages/studyboard/StudyBoardPage.tsx
@@ -7,26 +7,31 @@ import {
   Text,
   Button,
   Space,
+  Stack,
+  ActionIcon,
 } from "@mantine/core";
 import GridLayout from "react-grid-layout";
 import CustomHeader from "../../components/header/CustomHeader";
 import { Prism } from "@mantine/prism";
 import {
-  IconSettings,
-  IconMessageCircle,
-  IconPhoto,
-  IconSearch,
-  IconArrowsLeftRight,
-  IconTrash,
+  IconCards,
+  IconNotes,
+  IconFileTypePdf,
+  IconAlarm,
+  IconPhotoVideo,
   IconLayoutGridAdd,
+  IconCode,
+  IconPlus,
+  IconEqual,
 } from "@tabler/icons-react";
 import "./studyboardpage.scss";
+import ActionItemCard from "../../components/action_item/ActionItemCard";
 
 const StudyBoardPage = () => {
   const [layout, setLayout] = useState<GridLayout.Layout[]>([
     {
-      w: 3,
-      h: 16,
+      w: 12,
+      h: 3,
       x: 0,
       y: 0,
       i: "a",
@@ -34,29 +39,38 @@ const StudyBoardPage = () => {
       static: false,
     },
     {
-      w: 7,
-      h: 9,
-      x: 3,
-      y: 0,
+      w: 4,
+      h: 13,
+      x: 0,
+      y: 3,
       i: "b",
       moved: false,
       static: false,
     },
     {
-      w: 7,
-      h: 7,
-      x: 3,
-      y: 9,
+      w: 5,
+      h: 13,
+      x: 4,
+      y: 3,
       i: "c",
       moved: false,
       static: false,
     },
     {
-      w: 2,
+      w: 3,
       h: 1,
-      x: 10,
-      y: 0,
+      x: 9,
+      y: 3,
       i: "d",
+      moved: false,
+      static: false,
+    },
+    {
+      w: 3,
+      h: 6,
+      x: 9,
+      y: 4,
+      i: "e",
       moved: false,
       static: false,
     },
@@ -79,6 +93,7 @@ const StudyBoardPage = () => {
         <CustomHeader />
       </Header>
       <GridLayout
+        draggableHandle=".grid-item-drag"
         useCSSTransforms={false}
         className="studyboard-layout"
         layout={layout}
@@ -89,22 +104,45 @@ const StudyBoardPage = () => {
         onLayoutChange={(layout) => setLayout(layout)}
       >
         <Card key="a" className="grid-item-container">
+          <ActionIcon
+            c="dimmed"
+            className=" grid-item-drag grid-item-drag-handle"
+          >
+            <IconEqual />
+          </ActionIcon>
           <Prism
             colorScheme={theme.colorScheme}
             language="json"
             copyLabel="Copy code to clipboard"
             copiedLabel="Code copied to clipboard"
+            className="grid-item-content"
           >
             {JSON.stringify(layout, null, 2)}
           </Prism>
         </Card>
         <Card key="b" className="grid-item-container">
-          Something A
+          <ActionIcon
+            c="dimmed"
+            className=" grid-item-drag grid-item-drag-handle"
+          >
+            <IconEqual />
+          </ActionIcon>
+          <div className="grid-item-content">
+            <Text>Notes Section</Text>
+          </div>
         </Card>
         <Card key="c" className="grid-item-container">
-          Something B
+          <ActionIcon
+            c="dimmed"
+            className=" grid-item-drag grid-item-drag-handle"
+          >
+            <IconEqual />
+          </ActionIcon>
+          <div className="grid-item-content">
+            <Text>Flashcard Section</Text>
+          </div>
         </Card>
-        <Card key="d" className="grid-item-container">
+        <Card key="d" className=" grid-item-drag grid-item-container">
           <Menu shadow="md" trigger="hover" openDelay={100} closeDelay={400}>
             <Menu.Target>
               <Button fullWidth h={"100%"}>
@@ -126,26 +164,68 @@ const StudyBoardPage = () => {
               }}
             >
               <Menu.Label>Study Widgets</Menu.Label>
-              <Menu.Item icon={<IconSettings size={14} />}>
-                Flashcards
-              </Menu.Item>
-              <Menu.Item icon={<IconMessageCircle size={14} />}>
-                Notes
-              </Menu.Item>
-              <Menu.Item icon={<IconPhoto size={14} />}>PDF</Menu.Item>
-              <Menu.Item icon={<IconSearch size={14} />}>Timer</Menu.Item>
+              <Menu.Item icon={<IconCards size={14} />}>Flashcards</Menu.Item>
+              <Menu.Item icon={<IconNotes size={14} />}>Notes</Menu.Item>
+              <Menu.Item icon={<IconFileTypePdf size={14} />}>PDF</Menu.Item>
+              <Menu.Item icon={<IconAlarm size={14} />}>Timer</Menu.Item>
 
               <Menu.Divider />
 
               <Menu.Label>Extra Widgets</Menu.Label>
-              <Menu.Item icon={<IconArrowsLeftRight size={14} />}>
+              <Menu.Item icon={<IconPhotoVideo size={14} />}>
                 Video Player
               </Menu.Item>
-              <Menu.Item icon={<IconTrash size={14} />}>
+              <Menu.Item icon={<IconCode size={14} />}>
                 Code Playground
               </Menu.Item>
             </Menu.Dropdown>
           </Menu>
+        </Card>
+        <Card key="e" className="grid-item-container">
+          <ActionIcon
+            c="dimmed"
+            className="grid-item-drag grid-item-drag-handle"
+          >
+            <IconEqual />
+          </ActionIcon>
+          <div
+            className="grid-item-content action-item-container"
+            style={{
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            }}
+          >
+            <Stack spacing={"0.5rem"}>
+              <ActionItemCard
+                id="action-item-1"
+                title={"Action Item 1"}
+                description={"This is a description"}
+                completed={false}
+                draggable={false}
+                index={0}
+                thinkfolderColor="blue"
+              />
+              <ActionItemCard
+                id="action-item-2"
+                title={"Action Item 2"}
+                description={"This is a description"}
+                completed={false}
+                draggable={false}
+                index={1}
+                thinkfolderColor="pink"
+              />
+              <Button
+                onClick={() => console.log("Add Action Item")}
+                variant="default"
+                h={"3rem"}
+                leftIcon={<IconPlus size={"1rem"} />}
+              >
+                Add Action Item
+              </Button>
+            </Stack>
+          </div>
         </Card>
       </GridLayout>
     </div>
